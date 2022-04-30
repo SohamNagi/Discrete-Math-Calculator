bbfc7630b64d495a5bb6e9f485d38a56f84417fa
SohamNagi <nagisoham@gmail.com>
2022-04-30 02:07:09 -0400

    Co-authored-by: saadzaki7 <saadzaki7@users.noreply.github.com>

--- Text-Code.py
+++ Text-Code.py
@@ -1,5 +1,18 @@
-print("hello world")
+#import math
 
-print("zip")
+def gcd(x, y):
+  
+    if x > y:
+        small = y
+    else:
+        small = x
+    for i in range(1, small+1):
+        if((x % i == 0) and (y % i == 0)):
+            gcd = i
+              
+    return gcd
+  
+print(gcd(5, 10))
 
-print("ooga")
+def eea(x,y):
+    
