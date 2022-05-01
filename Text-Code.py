26406834f1b3fb65639368b72539a8e00d19a17b
SohamNagi <nagisoham@gmail.com>
2022-04-30 23:41:44 -0400

    Co-authored-by: saadzaki7 <saadzaki7@users.noreply.github.com>

--- Text-Code.py
+++ Text-Code.py
@@ -1,5 +1,9 @@
 #import math
 
+from math import floor
+from tabulate import tabulate
+
+
 def gcd(x, y):
   
     if x > y:
@@ -14,5 +18,31 @@ def gcd(x, y):
   
 print(gcd(5, 10))
 
+
+def next_row(row1, row2):
+    q = floor(row1[2]/row2[2])
+    a= row1[0]- (q * row2[0])
+    b = row1[1]- (q * row2[1])
+    r = row1[2]- (q * row2[2])
+    new_row = [a,b,r,q]
+    return new_row
+
+
 def eea(x,y):
+    row_1 = [1,0, x ,0]
+    row_2 = [0,1, y ,0]
+    data = [row_1,row_2]
+    a=0
+    b=1    
+    while(True):
+        new_row =  next_row(data[0], data[1])
+        if (new_row[2] == 2):
+            data.append(new_row)
+            break
+        else:
+            data.append(new_row)
+    
+    print (tabulate(data, headers=["a", "b", "r", "q"]))
+
+        
     
